d0058f57c12ff269c5c7927b428fd97d17910ba4
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-24 16:47:29 +0200

    click sulle bombe da fare

--- js/script.js
+++ js/script.js
@@ -8,11 +8,16 @@
 
 /** Consegna 2 **/
 // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 
 // Richiamo il container della griglia tramite il suo id
 const wrapContainer = document.getElementById('container-wrapper');
 
-// Creo un array vuoto per i numeri randomici che andranno aggiunti alla blacklist
 const blackList = [];
 
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
@@ -24,27 +29,31 @@ playButton.addEventListener('click', function () {
     // Svuoto il contenitore della griglia
     wrapContainer.innerHTML = '';
 
+    for (let i = 0; i < 15; i++) {
+        const newRandomNumber = generateRandomNumber(blackList, 1, 100);
+        console.log(newRandomNumber);
+    }
+
     // Creo un ciclo per generare la griglia
     for (let i = 1; i < 101; i++) {
         const newSquare = createNewSquare();
 
-        // Richiamo una variabile per aggiungere i numeri randomici alla blacklist
-        const newRandomNumber = generateRandomNumber(blackList, 1, 101);
-        blackList.push(newRandomNumber);
-
         // Aggiungo l'indice (i numeri) dentro al quadrato
-        newSquare.innerHTML = newRandomNumber;
+        newSquare.innerHTML = i;
 
         // Aggiungo il click al quadrato
         newSquare.addEventListener('click', function () {
-            newSquare.classList.add('active');
 
-            // Stampo in console il numero del quadrato cliccato
-            console.log(`Hai cliccato il numero ${i}`);
+            if () {
+                newSquare.classList.add('bomb');
+            } else {
+                newSquare.classList.add('active');
+            }
         })
 
         // Aggiungo la griglia al parent
         wrapContainer.append(newSquare);
+
     }
 })
 
@@ -56,18 +65,15 @@ function createNewSquare() {
 }
 
 // Creo una funzione per generare i numeri randomici
-function generateRandomNumber(blacklist, min, max) {
-
+function generateRandomNumber(blackList, min, max) {
     let randomNumber;
     let validNumber = false;
 
     while (validNumber === false) {
         randomNumber = Math.floor(Math.random() * (max - min) + min);
-        if (!blacklist.includes(randomNumber)) {
+        if (!blackList.includes(randomNumber)) {
             validNumber = true;
         }
-        return randomNumber;
     }
+    return randomNumber;
 }
-
-
